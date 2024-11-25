cbe268e71fb271faafe25c565fbbfbf2c383ccf9
Jinhyuk <kobin1970@gmail.com>
2024-11-25 18:14:09 +0100

    set up docker image build success

--- moun/src/main/java/io/moun/api/common/config/SecurityConfig.java
+++ moun/src/main/java/io/moun/api/common/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 허용할 출처 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost")); // 허용할 출처 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // 쿠키 및 인증 정보 허용
