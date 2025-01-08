7f171bbf6b6d1e78499904a50698c5c11cad4cf0
Jinhyuk <kobin1970@gmail.com>
2025-01-08 20:45:15 +0100

    test setting

--- moun/src/test/java/io/moun/api/MounApplicationTests.java
+++ moun/src/test/java/io/moun/api/MounApplicationTests.java
@@ -2,12 +2,14 @@ package io.moun.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes=MounApplication.class)
+@ActiveProfiles("test")
 class MounApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
